905bef7bbf507caab76a54085d9f25b90c742912
Mahmoud Abid <mahmoudabidup@gmail.com>
2025-02-04 22:48:17 +0100

    Home Screen First Implementation

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,10 +1,217 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({
+    super.key
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.all(8.0),
+            child: CircleAvatar(
+              backgroundImage: AssetImage("assets/images/profile.png"),
+            ),
+        ),
+        title: Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: "Haaa! ",
+                style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+              ),
+              TextSpan(
+                text: "Mahamed",
+                style: TextStyle(color: Colors.green, fontWeight: FontWeight.w700),
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          Icon(Icons.notifications, color: Colors.green),
+          SizedBox(width: 16),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.search),
+                  suffixIcon: Icon(Icons.filter_list),
+                  hintText: "Search...",
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+                ),
+              ),
+              SizedBox(height: 16),
+              Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Colors.green.withOpacity(0.1),
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Need Our Help?",
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(height: 8),
+                          Text("Feel free to contact our support for any troubles"),
+                          SizedBox(height: 8),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: Text("Call Now", style: TextStyle(fontSize: 12, color: Colors.white)),
+                            style: ElevatedButton.styleFrom(
+
+                              backgroundColor: Colors.green,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Image.asset(
+                      "assets/images/help.png",
+
+                      fit: BoxFit.cover,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Your Greenhouses",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "12 Places",
+                    style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            elevation: 4,
+                            child: Row(
+                              children: [
+                                Padding(padding: const EdgeInsets.only(left: 8)),
+                                ClipRRect(
+                                  borderRadius: BorderRadius.horizontal(
+                                    left: Radius.circular(8),
+                                    right: Radius.circular(8)
+                                  ),
+                                  child: Image.asset(
+                                    'assets/images/LandDemo.png',
+                                    height: 130,
+                                    width: 130,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                                Expanded(
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            "Maze Cultivation",
+                                            style: TextStyle(
+                                              fontSize: 16,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          SizedBox(height: 2),
+                                          Row(
+                                            children: [
+                                              Icon(
+                                                Icons.location_on,
+                                                size: 16, // Adjust size as needed
+                                                color: Color(0xFF777777), // Hex color for icon
+                                              ),
+                                              SizedBox(width: 4), // Add spacing between icon and text
+                                              Text(
+                                                "Sfax, Chaaleb",
+                                                style: TextStyle(
+                                                  fontSize: 12,
+                                                  color: Color(0xFF777777), // Hex color for text
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                          SizedBox(height: 2),
+                                          Text(
+                                            "Maze is a tropical plant which prefers warm humid weather.",
+                                            style: TextStyle(fontSize: 12),
+                                            maxLines: 2,
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          SizedBox(height: 4),
+                                          ElevatedButton(
+                                            onPressed: () {},
+                                            child: Text(
+                                              "Read Details",
+                                              style: TextStyle(fontSize: 12, color: Colors.white),
+                                            ),
+                                            style: ElevatedButton.styleFrom(
+                                              backgroundColor: Colors.green,
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(4),
+                                              ),
+                                            ),
+                                          ),
+                                        ],
+                                      ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                      );
+                    },
+                  ),
+                ),
+            ],
+          ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.store), label: "Magasin"),
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: "Regions"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+        selectedItemColor: Colors.green,
+        unselectedItemColor: Colors.grey,
+        onTap: (index) {
+          // Add navigation logic here
+        },
+      ),
+    );
   }
 }
